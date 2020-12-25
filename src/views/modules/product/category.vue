00d372ac7c3fc74aa99f764ef2d13e77e1cac41a
HanyiD <245714873@qq.com>
2020-12-25 17:40:17 +0800

    first commit

---
+++ src/views/modules/product/category.vue
@@ -0,0 +1,44 @@
+<!--  -->
+<template>
+  <el-tree
+    :data="data"
+    :props="defaultProps"
+    @node-click="handleNodeClick"
+  ></el-tree>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: [],
+      defaultProps: {
+        children: "children",
+        label: "label",
+      }
+    };
+  },
+  methods: {
+    handleNodeClick(data) {
+      console.log(data);
+    },
+    getMenus(){
+      this.$http({
+          url: this.$http.adornUrl('/product/category/list/tree'),
+          method: 'get'
+        }).then(data=>{
+            console.log("成功获取到数据。。",data)
+        })
+    }
+  },
+  //生命周期 - 创建完成（访问当前this实例）
+  created() {
+    this.getMenus();
+  },
+  //生命周期 - 挂载完成（访问DOM元素）
+  mounted() {},
+};
+</script>
+<style scoped>
+/* @import url(); 引入css类 */
+</style>
